41808f28196ea991e3d2b818ab61cc978c498e4b
wuchaopang <20141105049wch@mac23.local>
2015-12-09 19:47:43 +0800

    runover2
    改动第一个数组的大小，结果仍可以正常，在运行结果处也可以发现数组溢出

--- main.cpp
+++ main.cpp
@@ -10,17 +10,17 @@
 
 int main(int argc, const char * argv[])
 {
-    int a[3]={1,2,3};
+    int a[5]={1,2,3,4,5};
     int b[4];
     int *p;
     p=a;
     int i;
-    for(i=0;i<100;i++)
+    for(i=0;i<50;i++)
     {
         b[i]=i;
     }
-    printf("%d  %d  %d\n",a[0],a[1],a[2],a[3]);
-    printf("%d  %d  %d\n",&a[0],&a[1],&a[2],&a[3]);
+    printf("%d  %d  %d %d %d %d\n",a[0],a[1],a[2],a[3],a[4],a[5]);
+    printf("%d  %d  %d\n",&a[0],&a[1],&a[2]);
     printf("%d %d %d %d %d %d\n",p,&b[0],&b[1],&b[2],&b[3],b);
     return 0;
 }
